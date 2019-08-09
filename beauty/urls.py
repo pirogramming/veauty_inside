89e96af1d9ba6c0b21d8b2f2622bbc69d8f7c703
bumsu <bumsu0211@gmail.com>
2019-08-09 16:33:45 +0900

    view 리텍토링, form resubmission 방지

--- beauty/urls.py
+++ beauty/urls.py
@@ -7,6 +7,11 @@ urlpatterns = [
     path('', views.home, name="home"),
     path('video/<str:period>', views.video_list, name="video_list"),
     path('cosmetic/<str:kind>', views.cosmetic_list, name="cosmetic_list"),
+
     path('combinate/result', views.combine_result, name="combine_result"),
     path('combinate/<str:kind>', views.combine_cosmetic, name="combine_cosmetic"),
+    
+    path('pick', views.cosmetic_pick, name="cosmetic_pick"),
+    path('delete', views.cosmetic_delete, name="cosmetic_delete"),
+    path('reset', views.cosmetic_reset, name="cosmetic_reset"),
 ]
--- beauty/views.py
+++ beauty/views.py
@@ -5,11 +5,12 @@ from random import randint
 from .models import Youtuber, Video, Cosmetic, Bigcate, Smallcate #, Viuser
 import datetime
 
+# fix : global variable 'selected' User model의 필드로 교체
 selected = []
 
 def pagnation(request, contexts, PAGE_ROW_COUNT, PAGE_DISPLAY_COUNT, contexts_name):
     paginator=Paginator(contexts, PAGE_ROW_COUNT)
-    pageNum=request.GET.get('pageNum') # 현제 페이지
+    pageNum=request.GET.get('pageNum') # 현재 페이지
     
     totalPageCount=paginator.num_pages # 전체 페이지 갯수 
     
@@ -34,6 +35,7 @@ def pagnation(request, contexts, PAGE_ROW_COUNT, PAGE_DISPLAY_COUNT, contexts_na
 
     return {
         contexts_name: contexts,
+        'PAGE_ROW_COUNT' : PAGE_ROW_COUNT,
         'pageNum': pageNum,
         'bottomPages': bottomPages,
         'totalPageCount': totalPageCount,
@@ -41,8 +43,6 @@ def pagnation(request, contexts, PAGE_ROW_COUNT, PAGE_DISPLAY_COUNT, contexts_na
         'endPageNum': endPageNum,
     }
 
-
-# Create your views here.
 def home(request):
     #test db 생성
     '''
@@ -132,90 +132,55 @@ def video_list(request, period):
     
     return render(request, 'beauty/video_list.html', contexts)
 
+def list_for_cosmetic(request, kind):
+    bigcates = Bigcate.objects.all()
+
+    if kind == 'all':
+        cosmetics = Cosmetic.objects.annotate(count=Count('video')).order_by('-count')
+    else:
+        smallcate_eng_name = [smallcategory.eng_name for smallcategory in Smallcate.objects.all()]
+        if kind in smallcate_eng_name:
+            curr_smallcate = get_object_or_404(Smallcate, eng_name=kind)
+            curr_bigcate = get_object_or_404(Bigcate, pk=curr_smallcate.bigcate.pk)
+            smallcates = curr_bigcate.smallcate_set.all()
+            cosmetics = Cosmetic.objects.filter(category=curr_smallcate).annotate(count=Count('video')).order_by('-count')
+        else:
+            return 0
+
+    PAGE_ROW_COUNT = 10
+    PAGE_DISPLAY_COUNT = 10
+
+    contexts = pagnation(request, cosmetics, PAGE_ROW_COUNT, PAGE_DISPLAY_COUNT, 'cosmetics')
+    contexts['kind'] = kind
+    contexts['big_categories'] = bigcates
+
+    if kind != 'all':
+        contexts['curr_big'] = curr_bigcate
+        contexts['curr_small'] = curr_smallcate
+        contexts['small_categories'] = smallcates
+    
+    return contexts
+
 def cosmetic_list(request, kind):
-    bigcates = Bigcate.objects.all()
-
-    if kind == 'all':
-        cosmetics = Cosmetic.objects.annotate(count=Count('video')).order_by('-count')
+    contexts = list_for_cosmetic(request, kind)
+    if contexts == 0:
+        return redirect("beauty:home")
     else:
-        smallcate_eng_name = [smallcategory.eng_name for smallcategory in Smallcate.objects.all()]
-        if kind in smallcate_eng_name:
-            curr_smallcate = get_object_or_404(Smallcate, eng_name=kind)
-            curr_bigcate = get_object_or_404(Bigcate, pk=curr_smallcate.bigcate.pk)
-            smallcates = curr_bigcate.smallcate_set.all()
-            cosmetics = Cosmetic.objects.filter(category=curr_smallcate).annotate(count=Count('video')).order_by('-count')
-        else:
-            return redirect("beauty:home")
+        youtube_num = 5
+        contexts['yt_num'] = youtube_num
+        contexts['yt_range'] = range(youtube_num)
+        return render(request, 'beauty/cosmetic_list.html', contexts)
 
-    PAGE_ROW_COUNT = 10
-    PAGE_DISPLAY_COUNT = 10
-
-    contexts = pagnation(request, cosmetics, PAGE_ROW_COUNT, PAGE_DISPLAY_COUNT, 'cosmetics')
-    contexts['kind'] = kind
-    contexts['big_categories'] = bigcates
-
-    if kind != 'all':
-        contexts['curr_big'] = curr_bigcate
-        contexts['curr_small'] = curr_smallcate
-        contexts['small_categories'] = smallcates
-
-    return render(request, 'beauty/cosmetic_list.html', contexts)
-
-# fix : global variable 'selected' User model의 필드로 교체
 def combine_cosmetic(request, kind):
-    bigcates = Bigcate.objects.all()
-
-    if kind == 'all':
-        cosmetics = Cosmetic.objects.annotate(count=Count('video')).order_by('-count')
+    contexts = list_for_cosmetic(request, kind)
+    if contexts == 0:
+        return redirect("beauty:home")
     else:
-        smallcate_eng_name = [smallcategory.eng_name for smallcategory in Smallcate.objects.all()]
-        if kind in smallcate_eng_name:
-            curr_smallcate = get_object_or_404(Smallcate, eng_name=kind)
-            curr_bigcate = get_object_or_404(Bigcate, pk=curr_smallcate.bigcate.pk)
-            smallcates = curr_bigcate.smallcate_set.all()
-            cosmetics = Cosmetic.objects.filter(category=curr_smallcate).annotate(count=Count('video')).order_by('-count')
-        else:
-            return redirect("beauty:home")
+        contexts['selected'] = selected
+        return render(request, 'beauty/combine_cosmetic.html', contexts)
 
-    PAGE_ROW_COUNT = 10
-    PAGE_DISPLAY_COUNT = 10
-
-    contexts = pagnation(request, cosmetics, PAGE_ROW_COUNT, PAGE_DISPLAY_COUNT, 'cosmetics')
-    contexts['kind'] = kind
-    contexts['big_categories'] = bigcates
-
-    if kind != 'all':
-        contexts['curr_big'] = curr_bigcate
-        contexts['curr_small'] = curr_smallcate
-        contexts['small_categories'] = smallcates
-
+def cosmetic_pick(request):
     if request.method == "POST":
-        try:
-            request.POST['reset_selected']
-            selected.clear()
-            contexts['selected'] = selected
-
-            return render(request, 'beauty/combine_cosmetic.html', contexts)
-        except:
-            pass
-
-        try:
-            request.POST['delete_selected']
-            nums = []
-            for num in request.POST:
-                try:
-                    nums.append(int(num)-1)
-                except:
-                    pass
-            for num in sorted(nums, reverse=True):
-                del selected[num]
-
-            contexts['selected'] = selected
-
-            return render(request, 'beauty/combine_cosmetic.html', contexts)
-        except:
-            pass
-
         for num in request.POST:
             try:
                 selection = get_object_or_404(Cosmetic, pk=num)
@@ -223,37 +188,53 @@ def combine_cosmetic(request, kind):
                     selected.append(selection)
             except:
                 pass
-    
-    contexts['selected'] = selected
+        return redirect("beauty:combine_cosmetic", request.POST['kind'])
+    else:
+        return redirect("beauty:combine_cosmetic", 'all')
 
-    return render(request, 'beauty/combine_cosmetic.html', contexts)
+def cosmetic_delete(request):
+    if request.method == "POST":
+        nums = []
+        for num in request.POST:
+            try:
+                nums.append(int(num)-1)
+            except:
+                pass
+        for num in sorted(nums, reverse=True):
+            del selected[num]
 
+        return redirect("beauty:combine_cosmetic", request.POST['kind'])
+    else:
+        return redirect("beauty:combine_cosmetic", 'all')
+
+def cosmetic_reset(request):
+    if request.method == "POST":
+        selected.clear()
+        return redirect("beauty:combine_cosmetic", request.POST['kind'])
+    else:
+        return redirect("beauty:combine_cosmetic", 'all')
 
 def combine_result(request):
     if request.method == 'POST':
-        cosmetics = []
-        video_cnt = {}
-        for num in request.POST:
-            try:
-                cosmetic = get_object_or_404(Cosmetic, pk=num)
-                cosmetics.append(cosmetic)
+        video_infos = {}
+        for cosmetic in selected:
+            for video in cosmetic.video_set.all():
+                try:
+                    video_infos[video.id][1] = video_infos[video.id][1] + 1
+                except:
+                    video_info = [video.hits, 1]
+                    video_infos[video.id] = video_info
 
-                for video in cosmetic.video_set.all():
-                    try:
-                        video_cnt[video.id] = video_cnt[video.id] + 1
-                    except:
-                        video_cnt[video.id] = 1
-            except:
-                pass
-        print(len(video_cnt))
         videos = []
-        for video_id, cnt in sorted(video_cnt.items(), key=lambda t : t[1], reverse=True)[0:10]:
-            video = get_object_or_404(Video, pk=video_id)
+        recomend_videos = sorted(video_infos.items(), key=lambda t : (t[1][1], t[1][0]), reverse=True)[0:10]
+        print(recomend_videos)
+        for recomend_video in recomend_videos:
+            video = get_object_or_404(Video, pk=recomend_video[0])
             videos.append(video)
 
         return render(request, "beauty/combine_result.html", {
-            'cosmetics' : cosmetics,
+            'cosmetics' : selected,
             'videos' : videos,
         })
     else:
-        return redirect("beauty:combine_cosmetic")
+        return redirect("beauty:combine_cosmetic", 'all')
