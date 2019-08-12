9e9c4f13a22f6540703eb01eb6ada120df7fe63f
bumsu <bumsu0211@gmail.com>
2019-08-12 21:07:15 +0900

    Add video_scrap function and scrap url path

--- beauty/urls.py
+++ beauty/urls.py
@@ -6,12 +6,14 @@ app_name = 'beauty'
 urlpatterns = [
     path('', views.home, name="home"),
     path('video/<str:period>', views.video_list, name="video_list"),
+    path('video_scrap', views.video_scrap, name="video_scrap"),
+
     path('cosmetic/<str:kind>', views.cosmetic_list, name="cosmetic_list"),
 
     path('combinate/result', views.combine_result, name="combine_result"),
     path('combinate/<str:kind>', views.combine_cosmetic, name="combine_cosmetic"),
     
-    path('pick', views.cosmetic_pick, name="cosmetic_pick"),
-    path('delete', views.cosmetic_delete, name="cosmetic_delete"),
-    path('reset', views.cosmetic_reset, name="cosmetic_reset"),
+    path('cosmetic_pick', views.cosmetic_pick, name="cosmetic_pick"),
+    path('cosmetic_delete', views.cosmetic_delete, name="cosmetic_delete"),
+    path('cosmetic_reset', views.cosmetic_reset, name="cosmetic_reset"),
 ]
